5792b7d3cead06e16992a06de4de6fc1b46e46bf
龙昊 <604872657@qq.com>
2023-07-23 14:03:48 +0800

    dpi为160时，1dp = 1px

--- screen/src/main/java/com/jtl/screen/MainActivity.java
+++ screen/src/main/java/com/jtl/screen/MainActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
